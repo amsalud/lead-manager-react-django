278dd4e590929e01f2474f7b4d08ec0db66d7e3f
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-12 05:44:42 -0400

    Add UserAPI class

--- leadmanager/accounts/api.py
+++ leadmanager/accounts/api.py
@@ -19,6 +19,8 @@ class RegisterAPI(generics.GenericAPIView):
         })
 
 # Login API
+
+
 class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
@@ -26,9 +28,19 @@ class LoginAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
-        
+
         return Response({
             'user': UserSerializer(user, context=self.get_serializer_context()).data,
             'token': AuthToken.objects.create(user)
         })
+
 # Get User API
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
