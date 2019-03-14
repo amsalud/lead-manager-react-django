041e011fcd24d1abdb0a2bab959ba2a2832881aa
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-13 20:05:48 -0400

    Add functionality to register a user

--- leadmanager/ui/src/components/auth/Register.js
+++ leadmanager/ui/src/components/auth/Register.js
@@ -1,82 +1,107 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { register } from '../../actions/auth';
+import { createMessage } from '../../actions/messages';
+import PropTypes from 'prop-types';
 
 class Register extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    password_confirm: ''
+  };
 
-    state = {
-        username: '',
-        email: '',
-        password: '',
-        password_confirm: ''
-    };
+  static propTypes = {
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+  };
 
-    onSubmit = e => {
-        e.preventDefault();
-        console.log('submit')
+  onSubmit = e => {
+    e.preventDefault();
+    const { username, email, password, password_confirm } = this.state;
+    if (password !== password_confirm) {
+      this.props.createMessage({ passwordsNotMatch: 'Password do not match' });
+    } else {
+      const newUser = {
+        username,
+        email,
+        password
+      };
+      this.props.register(newUser);
     }
+  };
 
-    onChange = e => this.setState({ [e.target.name]: e.target.value })
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
 
-    render() {
-        const { username, email, password, password_confirm } = this.state;
-        return (
-            <div className="col-md-6 m-auto">
-                <div className="card card-body mt-5">
-                    <h2 className="text-center">Register</h2>
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <label>Username</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="username"
-                                onChange={this.onChange}
-                                value={username}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Email</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                name="email"
-                                onChange={this.onChange}
-                                value={email}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                name="password"
-                                onChange={this.onChange}
-                                value={password}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Confirm Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                name="password_confirm"
-                                onChange={this.onChange}
-                                value={password_confirm}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <button type="submit" className="btn btn-primary">
-                                Register
-                  </button>
-                        </div>
-                        <p>
-                            Already have an account? <Link to="/login">Login</Link>
-                        </p>
-                    </form>
-                </div>
+  render() {
+    const { username, email, password, password_confirm } = this.state;
+    return (
+      <div className="col-md-6 m-auto">
+        <div className="card card-body mt-5">
+          <h2 className="text-center">Register</h2>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                onChange={this.onChange}
+                value={username}
+              />
             </div>
-        )
-    }
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                onChange={this.onChange}
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                onChange={this.onChange}
+                value={password}
+              />
+            </div>
+            <div className="form-group">
+              <label>Confirm Password</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password_confirm"
+                onChange={this.onChange}
+                value={password_confirm}
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </div>
+            <p>
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Register;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { register, createMessage }
+)(Register);
